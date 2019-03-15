e3ff9659a13594ee767d52bf92417f4893af1ce1
davidCarba <da.carballal@gmail.com>
2019-03-15 20:48:04 +0100

    scss styles created + app.components minor changes

--- social-network/src/app/app.component.ts
+++ social-network/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'social-network';
+  title = 'Hola mundo';
+
+  constructor() {}
+
+  onClick(event) {
+    console.log(event);
+  }
 }
