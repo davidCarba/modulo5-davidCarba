efb6042502caec8626a2a943dca7fb3f3a000ba5
davidCarba <da.carballal@gmail.com>
2019-03-16 10:55:14 +0100

    [ADD] Routing

--- social-network/src/app/about/about-routing.module.ts
+++ social-network/src/app/about/about-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component:AboutComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  declarations: []
 })
-export class AboutRoutingModule { }
+
+export class AboutRoutingModule {}
--- social-network/src/app/about/about.module.ts
+++ social-network/src/app/about/about.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AboutComponent } from './components/about/about.component';
 import { AboutRoutingModule } from './about-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AboutRoutingModule
-  ]
+  imports: [CommonModule,AboutRoutingModule],
+  declarations: [AboutComponent]
 })
-export class AboutModule { }
+
+export class AboutModule {}
